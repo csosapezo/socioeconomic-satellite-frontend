598a6d2251fbe46ef23de8342012c00d610fcb84
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-04-13 18:17:26 -0500

    add general gui layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Sidebar from './sidebar'
+import Map from './map'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <Sidebar/>
+          <Map/>
+          <h6 className="footer"> </h6>
+        </div>
+    )
+  }
 }
 
 export default App;
