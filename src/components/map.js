94946fab9b15ea5af6a0ecc696d20d52ec5d673c
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-04-29 23:47:57 -0500

    convert map to function

--- src/components/map.js
+++ src/components/map.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import './map.css'
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer} from 'react-leaflet'
+import {MapContainer, TileLayer} from 'react-leaflet'
 
 const position = [-12.0453,  -77.0311]
 
-class Map extends React.Component {
-
-    render() {
-        return (
-            <div class="content">
-                <MapContainer center={position} zoom={11} scrollWheelZoom={false} style={{height: "100vh", width: "150vh"}}>
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                </MapContainer>
-            </div>
-        )
-    }
+function Map() {
+    return (
+        <div class="content">
+            <MapContainer center={position} zoom={11} scrollWheelZoom={false} style={{height: "100vh", width: "150vh"}}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
+    )
 }
 
 export default Map;
